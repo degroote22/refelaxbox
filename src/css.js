d859c614a7dafeaa9dc7745969fb5ad31ad538e2
degroote22 <lucaxx@gmail.com>
2017-06-13 11:06:47 -0300

    fix deps

--- src/css.js
+++ src/css.js
@@ -1,79 +1,81 @@
-const REG = /^([wmp][trblxy]?|flex|wrap|column|auto|align|justify|order)$/
+const REG = /^([wmp][trblxy]?|flex|wrap|column|auto|align|justify|order)$/;
 
 const css = config => props => {
-  const next = {}
-  const classNames = []
-  const renderer = config.renderer
+  const next = {};
+  const classNames = [];
+  const renderer = config.renderer;
 
-  const breaks = [null, ...config.breakpoints]
-  const sx = stylers(config)
+  const breaks = [null, ...config.breakpoints];
+  const sx = stylers(config);
   for (let key in props) {
-    const val = props[key]
+    const val = props[key];
     if (!REG.test(key)) {
-      next[key] = val
-      continue
+      next[key] = val;
+      continue;
     }
-    const cx = createRule(breaks, sx)(key, val)
-    classNames.push(renderer.renderRule(() => cx))
+    const cx = createRule(breaks, sx)(key, val);
+    classNames.push(renderer.renderRule(() => cx));
   }
 
-  next.className = join(...classNames, props.className)
+  next.className = join(...classNames, next.className);
 
-  return next
-}
+  return next;
+};
 
 const createRule = (breaks, sx) => (key, val) => {
-  const k = key.charAt(0)
-  const style = sx[key] || sx[k]
+  const k = key.charAt(0);
+  const style = sx[key] || sx[k];
 
-  const rules = toArr(val).map((v, i) => {
-    const bp = breaks[i]
-    const decs = style(key, v)
-    return media(bp, decs)
-  }).reduce((sum, n) => ({ ...sum, ...n }), {})
+  const rules = toArr(val)
+    .map((v, i) => {
+      const bp = breaks[i];
+      const decs = style(key, v);
+      return media(bp, decs);
+    })
+    .reduce((sum, n) => ({ ...sum, ...n }), {});
 
-  return rules
-}
+  return rules;
+};
 
-const toArr = n => Array.isArray(n) ? n : [n]
-const num = n => typeof n === 'number' && !isNaN(n)
+const toArr = n => (Array.isArray(n) ? n : [n]);
+const num = n => typeof n === "number" && !isNaN(n);
 
-const join = (...args) => args
-  .filter(a => !!a)
-  .join(' ')
+const join = (...args) => args.filter(a => !!a).join(" ");
 
-const dec = args => ({ [args[0]]: args[1] })
-const rule = args => args.reduce((sum, n) => ({ ...sum, ...n }), {})
-const media = (bp, body) => bp ? { [`@media screen and (min-width:${bp}em)`]: body } : body
+const dec = args => ({ [args[0]]: args[1] });
+const rule = args => args.reduce((sum, n) => ({ ...sum, ...n }), {});
+const media = (bp, body) =>
+  bp ? { [`@media screen and (min-width:${bp}em)`]: body } : body;
 
-const width = (key, n) => dec(['width', !num(n) || n > 1 ? px(n) : (n * 100) + '%'])
-const px = n => num(n) ? n + 'px' : n
+const width = (key, n) =>
+  dec(["width", !num(n) || n > 1 ? px(n) : n * 100 + "%"]);
+const px = n => (num(n) ? n + "px" : n);
 
 const space = scale => (key, n) => {
-  const [a, b] = key.split('')
-  const prop = a === 'm' ? 'margin' : 'padding'
-  const dirs = directions[b] || ['']
-  const neg = n < 0 ? -1 : 1
-  const val = !num(n) ? n : px((scale[Math.abs(n)] || Math.abs(n)) * neg)
-  return rule(dirs.map(d => dec([prop + d, val])))
-}
+  const [a, b] = key.split("");
+  const prop = a === "m" ? "margin" : "padding";
+  const dirs = directions[b] || [""];
+  const neg = n < 0 ? -1 : 1;
+  const val = !num(n) ? n : px((scale[Math.abs(n)] || Math.abs(n)) * neg);
+  return rule(dirs.map(d => dec([prop + d, val])));
+};
 
 const directions = {
-  t: ['Top'],
-  r: ['Right'],
-  b: ['Bottom'],
-  l: ['Left'],
-  x: ['Left', 'Right'],
-  y: ['Top', 'Bottom'],
-}
+  t: ["Top"],
+  r: ["Right"],
+  b: ["Bottom"],
+  l: ["Left"],
+  x: ["Left", "Right"],
+  y: ["Top", "Bottom"]
+};
 
-const flex = (key, n) => dec(['display', n ? 'flex' : 'block'])
-const wrap = (key, n) => dec(['flexWrap', n ? 'wrap' : 'nowrap'])
-const auto = (key, n) => dec(['flex', '1 1 auto'])
-const column = (key, n) => dec(['flexDirection', n ? 'column' : 'row'])
-const align = (key, n) => dec(['alignItems', n])
-const justify = (key, n) => dec(['justifyContent', n])
-const order = (key, n) => dec(['order', n])
+const flex = (key, n) => dec(["display", n ? "flex" : "block"]);
+const wrap = (key, n) => dec(["flexWrap", n ? "wrap" : "nowrap"]);
+const auto = (key, n) => dec(["flex", "1 1 auto"]);
+const column = (key, n) => dec(["flexDirection", n ? "column" : "row"]);
+const align = (key, n) => dec(["alignItems", n]);
+const justify = (key, n) => dec(["justifyContent", n]);
+const order = (key, n) => dec(["order", n]);
 
 const stylers = config => ({
   w: width,
@@ -86,6 +88,6 @@ const stylers = config => ({
   align,
   justify,
   order
-})
+});
 
-export default css
+export default css;
